c0f48c6d2af053caf9f83f5035db67df1e3a11c0
jeanne <jeannekengni1@gmail.com>
2026-07-01 17:09:22 +0100

    chore: supprimer les fichiers de cache du suivi Git

--- gestion-academique/storage/framework/views/21250300ea4672189a1a593ff3f7d521.php
+++
@@ -1,5 +0,0 @@
-
-<footer class="px-6 py-3 text-center text-xs text-slate-500 border-t border-slate-200 bg-white">
-    &copy; <?php echo e(date('Y')); ?> EduManager &bull; Projet académique &bull; Conçu avec
-    <i class="fa-solid fa-heart text-red-500"></i>
-</footer><?php /**PATH D:\projet_atelier_tp\gestion-academique\gestion-academique\resources\views/components/footer.blade.php ENDPATH**/ ?>
